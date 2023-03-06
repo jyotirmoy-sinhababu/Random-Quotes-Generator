a767e1a03582b234083cadde5c903bc8178e2ce3
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-03-06 23:29:44 +0530

    home page created

---
+++ src/pages/container/Cnt.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Cnt = () => {
+  return <div>Cnt</div>;
+};
+
+export default Cnt;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('https://quote-garden.onrender.com/api/v3/quotes').then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <p>home</p>
+    </div>
+  );
 };
 
 export default Home;
