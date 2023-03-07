331add2f4d9a7977164eccf49fd9421e464de658
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-03-07 23:19:28 +0530

    changes made

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,10 +5,12 @@ import axios from 'axios';
 const Home = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get('https://quote-garden.onrender.com/api/v3/quotes').then((res) => {
-      console.log(res.data);
-      setData(res.data);
-    });
+    axios
+      .get('https://quote-garden.onrender.com/api/v3/quotes?author=Bill Cosby')
+      .then((res) => {
+        console.log(res.data.data);
+        setData(res.data);
+      });
   }, []);
 
   return (
